a9708a86b3e4d97a9bf18ac022ac372ff9eac3df
Bekbolsun Raimberdiyev <bekbolsun.raimberdiev@gmail.com>
2023-11-02 13:31:05 +0100

    Finilized the Lombok

--- src/task2/Car.java
+++ src/task2/Car.java
@@ -8,9 +8,11 @@ import lombok.*;
 @Setter
 @Getter
 @NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Car {
 
     private String model;
+    @EqualsAndHashCode.Include
     private String brand;
     private int year;
     private double price;
@@ -23,4 +25,17 @@ public class Car {
     public void sell(int quantity){
         this.quantity -= quantity;
     }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Car car = (Car) o;
+//        return brand.equals(car.brand);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(brand);
+//    }
 }
--- src/task2/Main.java
+++ src/task2/Main.java
@@ -2,7 +2,7 @@ package task2;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("X","Tesla",2023,40000,"white", 10);
+        Car car = new Car("X","Toyota",2023,40000,"white", 10);
         Car car1 = new Car("Land Cruiser","Toyota",2020,50000,"Black",20);
         System.out.println(car);
         System.out.println(car1);
